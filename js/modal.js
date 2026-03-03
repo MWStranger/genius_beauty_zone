40293066a72d86281d2a016264e4fc83ee3c5e4d
Yevhen Sydelnykov <mwstrangercore@gmail.com>
2026-03-03 20:40:20 +0100

    STATE-IX JavaScript - Mobile menu

---
+++ js/modal.js
@@ -0,0 +1,57 @@
+// Блокування скролу сторінки при відкритті модального вікна
+document.body.classList.add('is-locked');
+document.body.classList.remove('is-locked');
+// Функції для блокування та розблокування скролу сторінки
+const lockScroll = () => {
+  const scrollBarWidth = window.innerWidth - document.documentElement.clientWidth;
+  document.body.style.paddingRight = `${scrollBarWidth}px`;
+  document.body.classList.add('is-locked');
+};
+
+const unlockScroll = () => {
+  document.body.style.paddingRight = '';
+  document.body.classList.remove('is-locked');
+};
+
+
+// Модуль для відкриття/закриття мобільного меню та модального вікна
+const mobileMenu = document.querySelector('.mobile-menu');
+const modal = document.querySelector('.backdrop');
+
+function open(el) {
+  el.classList.remove('is-hidden');
+  el.classList.add('is-open');
+}
+
+function close(el) {
+  el.classList.remove('is-open');
+  el.classList.add('is-hidden');
+}
+
+document.addEventListener('click', (e) => {
+  const actionEl = e.target.closest('.js-action');
+  if (!actionEl) return;
+
+  const action = actionEl.dataset.action;
+
+  if (action === 'menu') {
+    if (mobileMenu.classList.contains('is-open')) {
+      close(mobileMenu);
+      unlockScroll();
+    } else {
+      open(mobileMenu);
+      lockScroll();
+    }
+  }
+
+  if (action === 'modal') {
+    if (modal.classList.contains('is-open')) {
+      close(modal);
+      unlockScroll();
+    } else {
+      open(modal);
+      lockScroll();
+    }
+  }
+});
+
